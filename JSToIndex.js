88774d4f181d8e46e4c9680282cb06509594bfae
AlecMcAwesome <fredi-petersen93@hotmail.com>
2017-05-19 00:29:25 +0200

    Updated all going to add a insert button to mongoDB

--- JSToIndex.js
+++ JSToIndex.js
@@ -1,5 +1,12 @@
+var mongo = require('mongodb');
+var sUrl = "mongodb://localhost:27017/game";
+
 $(document).ready(function(){
+
     $('#inButton').click(function(){
+        var inName = $('#nameInput');
+        var inAge = $('#ageInput');
+
 
     })
 });
